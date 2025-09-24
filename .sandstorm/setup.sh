e695289bfe2879ec371e5b88f823c8911321f75b
Marcus Yu <saguiau4417@gmail.com>
2025-09-24 17:29:00 +0600

    feat: Download and extract Open-EMR
    This change installs the Open-EMR dependencies, downloads and extracts
    Open-EMR 7.0.3.  It also modifies the MariaDB configuration to minimize
    disk use.

---
+++ .sandstorm/setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# When you change this file, you must take manual action. Read this doc:
+# - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
+
+set -o errexit -o errtrace -o nounset -o pipefail
+# This is the ideal place to do things like:
+#
+#    export DEBIAN_FRONTEND=noninteractive
+#    apt-get update
+#    apt-get install -y nginx nodejs nodejs-legacy python2.7 mysql-server
+#
+# If the packages you're installing here need some configuration adjustments,
+# this is also a good place to do that:
+#
+#    sed --in-place='' \
+#            --expression 's/^user www-data/#user www-data/' \
+#            --expression 's#^pid /run/nginx.pid#pid /var/run/nginx.pid#' \
+#            --expression 's/^\s*error_log.*/error_log stderr;/' \
+#            --expression 's/^\s*access_log.*/access_log off;/' \
+#            /etc/nginx/nginx.conf
+
+# By default, this script does nothing.  You'll have to modify it as
+# appropriate for your application.
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+. "${script_dir}/environment"
+
+# Install Open-EMR dependencies
+export DEBIAN_FRONTEND=noninteractive
+apt-get install --yes apache2 libapache2-mod-php libtiff-tools php php-mysql php-cli php-gd php-xml php-curl php-soap php-json imagemagick php-mbstring php-zip php-ldap php-intl mariadb-server
+
+# Install development tools
+apt-get install --yes patch
+
+# Work with downloads
+mkdir -p "${DOWNLOADS_DIR}"
+cd "${DOWNLOADS_DIR}"
+
+# Download Open-EMR
+if [ ! -f "${OPENEMR_ARCHIVE}" ]; then
+	printf "%s\n" "Downloading Open-EMR ${OPENEMR_VERSION} from ${OPENEMR_URL}"
+	curl -vL "${OPENEMR_URL}" > "${DOWNLOADS_DIR}/${OPENEMR_ARCHIVE}"
+fi
+
+# Verify Open-EMR download
+printf "Verifying SHA-256 hash of Open-EMR archive\n"
+sha256sum --check --ignore-missing "${OPENEMR_ARCHIVE}.sha256"
+
+# Extract Open-EMR
+printf "Extracting Open-EMR\n"
+mkdir "${OPENEMR_OPT_DIR}"
+cd "${OPENEMR_OPT_DIR}"
+tar zxf "${DOWNLOADS_DIR}/${OPENEMR_ARCHIVE}"
+
+# Stop and disable services.  Sandstorm will run them.
+systemctl stop apache2
+systemctl stop mariadb
+systemctl disable apache2
+systemctl disable mariadb
+
+# Update MariaDB configuration
+patch /etc/mysql/mariadb.conf.d/50-server.cnf "${PATCHES_DIR}/mariadb-50-server.cnf.patch"
+exit 0
